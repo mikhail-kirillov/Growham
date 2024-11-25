316d58d7e524b52522f1a09ff2ed0d33ece7dc5b
Mikhail Kirillov <kirillov.mikhail.job@icloud.com>
2024-11-26 00:42:28 +0300

    delete all

--- core/src/gd/rf/theoneboringmancompany/growham/tools/AndroidConnector.java
+++
@@ -1,6 +0,0 @@
-package gd.rf.theoneboringmancompany.growham.tools;
-
-public interface AndroidConnector {
-    void insert(String Name, int Time);
-    Object[] selectToScore();
-}
--- core/src/gd/rf/theoneboringmancompany/growham/tools/MyAssetsManagers.java
+++
@@ -1,9 +0,0 @@
-package gd.rf.theoneboringmancompany.growham.tools;
-
-import com.badlogic.gdx.assets.AssetManager;
-
-public abstract class MyAssetsManagers {
-    public static AssetManager musicManager = new AssetManager();
-    public static AssetManager soundManager = new AssetManager();
-    public static AssetManager textureAtlasManager = new AssetManager();
-}
